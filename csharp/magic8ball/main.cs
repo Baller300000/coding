c74285bfa78bf157b962d25816afdff854eac606
Baller300000 <lvyssoulis@outlook.com>
2026-02-17 15:44:34 +0200

    do shit

--- csharp/magic8ball/main.cs
+++ csharp/magic8ball/main.cs
@@ -6,12 +6,8 @@ class Magic8Ball
     private static readonly List<string> Responses = new()
     {
         // GOOD BOIS :3
-        "do it and ill give you a cookie",
         "I dont care about details, FUCK YEAAHH",
         "sure bro why not",
-        "BULLSHIT. Do it now",
-        "If you dont do it ill decapitate you",
-        "DO IT",
         "FUCK YES",
         "The council of fuck yeah will decide",
         "FLOOR IT",
@@ -20,18 +16,15 @@ class Magic8Ball
         // IDK
         "LET ME THINK IN FUCKING PEACE",
         "FUCK IM SUPPOSED TO KNOW",
-        "NUH UH",
         "SHUT UP",
         "GO COMMIT DIE",
         
         // NOPE
-        "Don't count on MY FUCKING ASS",
         "My reply is FUCK NO",
-        "do that shit, il end you",
         "That would be FUCKING STUPID",
         "FUCK NO",
         "ABSOLUTELY FUCKING NOT",
-        "DO THAT AND ILL TOUCH U"
+        "ILL TOUCH U"
     };
 
     static void Main()
@@ -53,7 +46,7 @@ class Magic8Ball
                 Console.WriteLine("\nFUCK OFF");
                 break;
             }
-            // ----------
+            // --------------------
             // this is a joke, dont take seriously!!!!
             if (input.Equals("seig", StringComparison.OrdinalIgnoreCase)
             || input.Equals("heil", StringComparison.OrdinalIgnoreCase)
@@ -64,13 +57,15 @@ class Magic8Ball
                 continue;
             }
             if (input.Equals("is it gay to like femboys?", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("is it gay to like femboys", StringComparison.OrdinalIgnoreCase))
+            || input.Equals("is it gay to like femboys", StringComparison.OrdinalIgnoreCase)
+            || input.Equals("Is it gay to like femboys", StringComparison.OrdinalIgnoreCase)
+            || input.Equals("Is it gay to like femboys?", StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nU like them bc they look like women, then no, \nbut if u like theb bc of their personality then yes, mr weirdo");
                 continue;
             }
             // this is a joke, dont take seriously!!!!
-            // ----------
+            // --------------------
             if (input.IsWhiteSpace() || string.IsNullOrEmpty(input))
             {
                 Console.WriteLine("\nASK A QUESTION YOU FUCKING IDIOT");
@@ -78,13 +73,7 @@ class Magic8Ball
             }
 
             Console.WriteLine("\nThe Magic 8 Ball is thinking...");
-            for (int i = 0; i < 5; i++)
-            {
-                System.Threading.Thread.Sleep(300);
-                Console.Write(".");
-            }
             Console.WriteLine();
-
             // Get random response
             string response = Responses[random.Next(Responses.Count)];
             Console.WriteLine($"\n {response} \n");
