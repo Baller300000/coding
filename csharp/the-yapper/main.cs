7964b13a889f6c265441e18cbb31a1edc3a94212
Baller300000 <lvyssoulis@outlook.com>
2026-02-20 10:05:33 +0200

    DEUS VULT

--- csharp/the-yapper/main.cs
+++ csharp/the-yapper/main.cs
@@ -1,5 +1,7 @@
 namespace System;
 
+using System.Collections.Generic;
+
 class Magic8Ball
 {
     private static readonly List<string> philosophy = new()
@@ -42,13 +44,21 @@ class Magic8Ball
     };
     private static readonly List<string> eviljokes = new()
     {
-        "Where did little Timmy go after getting lost in a minefield? Everywhere.\n",
-        "What's the difference between a snowman and a snowwoman? Snowballs.\n",
-        "What did the cannibal get when he was late for dinner? The cold shoulder.\n",
-        "What did the Nazi say when he was asked to stop? 'I can't stop, I'm on a roll!'\n",
-        "Nock nock\nWho's there?\nVE VILL ASK ZE KVUESTIONZ\n",
-        "Why did 9/11 happen? Because the Twin Towers were too tall and needed to be brought down.\n",
-        "What do you call a man with no arms and no legs in a pool? Bob.\n"
+        "Where did little Timmy go when he got lost in a minefield? Everywhere.\n",
+        "Why did the orphan get hit by a car? Because he was an orphan.\n",
+        "Why did the serial killer go to therapy? To work on his issues.\n",
+        "What do you call a group of cannibals who eat a clown? A laughing stock.\n",
+        "Why did the psychopath break up with his girlfriend? Because she was too normal.\n",
+        "What is the difference between a snowman and a snowwoman? Snowballs.\n",
+        "Why did the necrophiliac break up with his girlfriend? Because she was already dead.\n",
+        "What do you call a group of evil clowns? A circus of doom.\n",
+        "Why did the serial killer go to the grocery store? To pick up some fresh victims.\n",
+        "What do you call a group of evil scientists? A lab of horrors.\n",
+        "Why did the cannibal break up with his girlfriend? Because she was too tasty.\n",
+        "What do you call a group of evil lawyers? A bar of evil.\n",
+        "Why did the psychopath go to the doctor? To get a check-up on his sanity.\n",
+        "What do you call a group of evil politicians? A senate of corruption.\n",
+        "Why did 9/11 happen? Because of the weight of YO MAMA.\n",
     };
     static void Main()
     {
@@ -65,7 +75,7 @@ class Magic8Ball
             Console.Write("Let's talk (/help for more info): ");
             string? input = Console.ReadLine();
 
-            if (input.Equals("/help", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/help", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nCommand list:");
                 Console.WriteLine("/help - Show this help message\n");
@@ -77,72 +87,72 @@ class Magic8Ball
                 Console.WriteLine("just talk to me for anything\n");
                 continue;
             }
-            if (input.Equals("/quit", StringComparison.OrdinalIgnoreCase))
+            if (!string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine(input.Length);
+            }
+
+            if (string.Equals("/quit", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nAight bye");
                 break;
             }
-            if (input.Equals("/saymyname", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/saymyname", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nWalt: You all know exactly who I am. Say my name.\nDeclan: Do it? I don't have a damn clue who the hell you are.\nWalt: Yeah, you do. I'm the cook. I'm the man who killed Gus Fring.\nDeclan: [pauses] Heisenberg.\nWalt: You're goddamn right.\n");
                 continue;
             }
-            if (input.Equals("/philosophy", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/philosophy", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\n" + philosophy[random.Next(0, philosophy.Count)] + "\n");
                 continue;
             }
-            if (input.Equals("/jokes dad", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/jokes dad", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\n" + dadjokes[random.Next(0, dadjokes.Count)] + "\n");
                 continue;
             }
-            if (input.Equals("/jokes evil", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/jokes evil", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\n" + eviljokes[random.Next(0, eviljokes.Count)] + "\n");
                 continue;
             }
-            if (input.Equals("/jokes", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("/jokes", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nHavent made that yet\n");
             }
             // --------------------
             // this is a joke, dont take seriously!!!
-            if (input.Equals("seig", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("heil", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("SEIG", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("HEIL", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("seig", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("heil", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("SEIG", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("HEIL", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nHEIL! (joking ofc)\n");
                 continue;
             }
             // this is a joke, dont take seriously!!!
-            if (input.Equals("is it gay to like femboys?", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("is it gay to like femboys", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("Is it gay to like femboys", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("Is it gay to like femboys?", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("is it gay to like femboys?", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("is it gay to like femboys", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("Is it gay to like femboys", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("Is it gay to like femboys?", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\nU like them bc they look like women, then no, \nbut if u like theb bc of their personality then yes, mr weirdo\n");
                 continue;
             }
             // this is a joke, dont take seriously!!!
-            if (input.Equals("\ndeus vult", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("\nDEUS VULT", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("\nDeus Vult", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("\ndeus vult!", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("\nDEUS VULT!", StringComparison.OrdinalIgnoreCase)
-            || input.Equals("\nDeus Vult!", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals("\ndeus vult", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("\nDEUS VULT", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("\nDeus Vult", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("\ndeus vult!", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("\nDEUS VULT!", input, StringComparison.OrdinalIgnoreCase)
+            || string.Equals("\nDeus Vult!", input, StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("\n DEUS VULT\n");
                 continue;
             }
             // this is a joke, dont take seriously!!!
             // --------------------
-            if (input.IsWhiteSpace() || string.IsNullOrEmpty(input))
-            {
-                Console.WriteLine("\nSAY MY NAME");
-                continue;
-            }
 
             Console.WriteLine("\nTalk to me... (I have nothing to say back tho)\n");
             Console.WriteLine();
